9a0cc0e68835b8b60cf312cae4d8e9cad3193ff6
kkc <bushwick97@naver.com>
2025-06-06 12:29:53 +0900

    config commit

--- src/main/java/com/gaeko/gamecut/config/MailConfig.java
+++ src/main/java/com/gaeko/gamecut/config/MailConfig.java
@@ -1,4 +1,4 @@
-package com.gaeko.gamecut;
+package com.gaeko.gamecut.config;
 
 import kr.co.youiwe.webservice.BitSms;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-public class SpringConfig {
+public class MailConfig {
     @Bean
     public BitSms sms() {
         return new BitSms();
