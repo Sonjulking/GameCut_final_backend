a6230faefed3a0ae83678e1fa57165fef8ba86e8
jeonhj1015 <mumu097@naver.com>
2025-06-16 20:02:21 +0900

    <feat> 댓글 기능

--- src/main/java/com/gaeko/gamecut/controller/CommentController.java
+++ src/main/java/com/gaeko/gamecut/controller/CommentController.java
@@ -5,12 +5,13 @@ import com.gaeko.gamecut.dto.CommentDTO;
 import com.gaeko.gamecut.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -27,4 +28,5 @@ public class CommentController {
         return ResponseEntity.ok(commentService.save(commentDTO));
     }
 
+    
 }
