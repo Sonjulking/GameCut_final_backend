c9291ca99b73ba59280b15e382aa8238e9d736b7
jeonhj1015 <mumu097@naver.com>
2025-06-30 21:14:13 +0900

    <feat> 댓글 삭제, 수정기능

--- src/main/java/com/gaeko/gamecut/service/CommentService.java
+++ src/main/java/com/gaeko/gamecut/service/CommentService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -46,6 +47,16 @@ public class CommentService {
     public List<CommentDTO> getCommentsByUser(Integer userNo) {
         List<Comment> comments = commentRepository.findByUserNo(userNo);
         return comments.stream().map(commentMapper::toDTO).toList();
+    }
+
+    @Transactional
+    public void deleteComment(Integer commentNo) {
+        commentRepository.deleteByCommentNo(commentNo);
+    }
+
+    @Transactional
+    public void updateComment(Integer commentNo, CommentDTO commentDTO) {
+        commentRepository.updateComment(commentNo, commentDTO);
     }
 
 
