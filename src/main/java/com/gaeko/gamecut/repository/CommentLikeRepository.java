071b8ede40df33dde34fb68199d5bd667a6952cc
jeonhj1015 <mumu097@naver.com>
2025-07-03 14:38:20 +0900

    <fix> 좋아요 기능 수정

--- src/main/java/com/gaeko/gamecut/repository/CommentLikeRepository.java
+++ src/main/java/com/gaeko/gamecut/repository/CommentLikeRepository.java
@@ -2,6 +2,8 @@
 package com.gaeko.gamecut.repository;
 
 import com.gaeko.gamecut.entity.CommentLike;
+import com.gaeko.gamecut.entity.CommentLikeId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Integer> {
+public interface CommentLikeRepository extends JpaRepository<CommentLike, CommentLikeId> {
     
     // 댓글 좋아요 존재 여부 확인
     boolean existsByUserUserNoAndCommentCommentNo(Integer userNo, Integer commentNo);
