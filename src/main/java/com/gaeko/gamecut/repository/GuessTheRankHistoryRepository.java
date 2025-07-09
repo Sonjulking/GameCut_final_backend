02dc1397c57ace2f84a3fed7979b6f9da99bd602
jeonhj1015 <mumu097@naver.com>
2025-07-09 14:41:59 +0900

    <feat> 내 게스더 랭크 기록 구현

--- src/main/java/com/gaeko/gamecut/repository/GuessTheRankHistoryRepository.java
+++ src/main/java/com/gaeko/gamecut/repository/GuessTheRankHistoryRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gaeko.gamecut.entity.GuessTheRankHistory;
+import java.util.List;
 
 @Repository
-public interface GuessTheRankHistoryRepository extends JpaRepository<GuessTheRankHistory, Integer> {}
+public interface GuessTheRankHistoryRepository extends JpaRepository<GuessTheRankHistory, Integer> {
+    // 2025-07-09 수정됨 - 사용자별 게임 기록 조회 (최신 순)
+    List<GuessTheRankHistory> findByUser_UserNoOrderBySolveDateDesc(Integer userNo);
+}
