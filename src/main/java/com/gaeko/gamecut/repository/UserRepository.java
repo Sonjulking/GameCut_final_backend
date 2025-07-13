b9e68ad52a078a9f63e57c2276c5e7c8b6ff6168
kkc <bushwick97@naver.com>
2025-07-13 13:12:32 +0900

    .

--- src/main/java/com/gaeko/gamecut/repository/UserRepository.java
+++ src/main/java/com/gaeko/gamecut/repository/UserRepository.java
@@ -23,7 +23,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Modifying  // 이게 핵심!
     @Transactional  // 트랜잭션 추가
-    @Query(value = "UPDATE USER_TB SET USER_DELETE_DATE = SYSTIMESTAMP WHERE USER_ID = :userId", nativeQuery = true)
+    @Query(value = "UPDATE USER_TB SET USER_DELETE_DATE = NOW() WHERE USER_ID = :userId", nativeQuery = true)
     void userDelete(@Param("userId") String userId);
 
     //문자전송위한 함수
